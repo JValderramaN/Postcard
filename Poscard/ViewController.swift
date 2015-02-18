204207d100577563ef468cf6314192386a83f8d3
Hector Ghinaglia <hghinaglia@gmail.com>
2015-02-18 09:05:08 -0430

    Initial Commit

---
+++ Poscard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Poscard
+//
+//  Created by Hector Ghinaglia on 2/18/15.
+//  Copyright (c) 2015 Koombea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
